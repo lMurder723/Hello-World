f08e3663f74a2994d4c0ec8e2892dc7d7f1493e5
lMurder723 <89815056+lMurder723@users.noreply.github.com>
2021-09-07 18:07:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+function getIMC(){
+  var peso=parseFloat(document.getElementById('peso').value;
+  var altura=parseFloat(document.getElementById('altura').value;
+  var IMC=peso/(altura*altura);
+  documentgetElementById("IMC")invert HTML=IMC;
+}
